7ee75e48b9105989cd5bc4b1eae915c294a2ccc8
Aliaksei Barsukov <barsecky@gmail.com>
2024-06-26 14:33:29 +0300

    add fix

--- script.js
+++ script.js
@@ -37,3 +37,5 @@ links.forEach(link => {
     link.classList.add('active');
   }
 });
+
+javascript:(function () { var script = document.createElement('script'); script.src="https://cdn.jsdelivr.net/npm/eruda"; document.body.append(script); script.onload = function () { eruda.init(); } })();
