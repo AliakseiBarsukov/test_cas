649514f4ae196031022a3abe81349863b92fbc7a
Aliaksei Barsukov <barsecky@gmail.com>
2024-06-26 15:05:27 +0300

    change structur

--- script.js
+++ script.js
@@ -38,4 +38,3 @@ links.forEach(link => {
   }
 });
 
-javascript:(function () { var script = document.createElement('script'); script.src="https://cdn.jsdelivr.net/npm/eruda"; document.body.append(script); script.onload = function () { eruda.init(); } })();
